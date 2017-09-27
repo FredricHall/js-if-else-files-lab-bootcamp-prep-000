f92ad7bcbe4298f5fac48a4b86ac2d05b0266fda
Fredric Hall <fredric_hall@yahoo.com>
2017-09-27 07:56:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ if (name==="Susan"){
 } else {
   console.log("Expected the name to be Susan")
 }
-
+ if (height==="74") {
+   "Height is 74"
+ } else {
+   "Expected to be 74"
+ }
 // Don't worry about this
 module.exports = { name, height
 }
